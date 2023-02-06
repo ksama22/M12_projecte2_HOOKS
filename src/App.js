42618fb945328b93114d40bae26999da44744154
ksama22 <ksama22@jaumebalmes.net>
2023-02-06 16:18:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //Hooks
+  const [age, setAge] = useState(0);
+
+  const serMasViejo = () => {
+    setAge(age + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{age} años</div>
+      <div>
+        <button onClick={serMasViejo}>Ser mas viejo</button>
+      </div>
     </div>
   );
 }
